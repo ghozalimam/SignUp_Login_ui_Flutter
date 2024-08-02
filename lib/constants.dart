2cdf2f17641c0df1493e90bffc5258aa549eefed
ghozalimam <ghozal.perdana02@gmail.com>
2024-08-02 17:42:20 +0700

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xff92c7cf);
+const kPrimaryLightColor = Color(0xfff3f9fb);
---
+++ lib/widgets/rounded_icon.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class RoundedIcon extends StatelessWidget {
+  const RoundedIcon({Key? key, required this.imageUrl}) : super(key: key);
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Container(
+        width: 40,
+        height: 40,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.white,
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black26,
+              offset: Offset(0, 2),
+              blurRadius: 6.0,
+            ),
+          ],
+          image: DecorationImage(
+            image: AssetImage(imageUrl),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
